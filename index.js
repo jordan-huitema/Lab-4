e890a54e29edbcfab1409f38013ef4020598e27a
Jordan Huitema <Jordan-Huitema@live.nmit.ac.nz>
2020-04-07 18:11:35 +1200

    finished code and added comments

---
+++ index.js
@@ -0,0 +1,16 @@
+//Lab Four
+//Jordan Huitema
+
+function binary(num) {              //input number value
+    console.log(num.toString(2))    //convert number value to binary string
+}
+
+//the .toString() operator allows you to convert a string into 3 diffrent values
+// Binary           .toString(2)
+// Octal            .toString(8)
+// Hexadecimal      .toString(16)
+
+//reading up on the .toString() operator has made this test very very easy 
+
+binary(10);
+
